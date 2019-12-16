356056614b7db8b124161926e48f8a0eda3bb890
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-12-16 14:13:43 -0700

    status to open/closed

--- client/src/components/BugComponent.vue
+++ client/src/components/BugComponent.vue
@@ -2,23 +2,21 @@
   <tr class="bugComponent">
     <td>{{bugProp.title}}</td>
     <td>{{bugProp.reportedBy}}</td>
-    <td id="status">{{bugProp.closed}}</td>
+    <td>{{this.status}}</td>
     <td>{{bugProp.createdAt}}</td>
   </tr>
 </template>
 <script>
 export default {
   name: "bug",
+  data() {
+    return {
+      status: this.bugProp.closed ? "closed" : "open"
+    };
+  },
   computed: {
     bugs() {
       return this.$store.state.bugs;
-    }
-  },
-  methods: {
-    status() {
-      if (bugProp.closed == false) {
-        bugProp.closed = "open";
-      }
     }
   },
   props: ["bugProp"]
